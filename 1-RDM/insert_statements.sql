f503c7e9e2426dcfe391fbb495d91eef8a5f96ec
Andrew Braun <abraunkp@gmail.com>
2024-05-28 18:36:23 -0700

    Added yfinance functionality with tables

---
+++ 1-RDM/insert_statements.sql
@@ -0,0 +1,3 @@
+INSERT INTO stock (StockID, Ticker, Sector, Price, SD, ERet) VALUES (1, 'TSLA', 'Consumer Cyclical', 173.74, 2.408, 3.31);
+INSERT INTO stock (StockID, Ticker, Sector, Price, SD, ERet) VALUES (2, 'AAPL', 'Technology', 186.88, 1.264, 7.23);
+INSERT INTO stock (StockID, Ticker, Sector, Price, SD, ERet) VALUES (3, 'VTI', 'Unknown', 259.99, 0.0, 0.0);
